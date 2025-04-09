34632bfadc6e58ae092456099ce88e3932d657be
Vega Setiawan <vegasetiawan16@gmail.com>
2025-04-09 09:18:03 +0700

    initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+      <div className='text-[var(--font-color)] text-7xl h-screen w-screen flex flex-col justify-center items-center'>
+        <div className='text-center'>
+          <h1>Greetings
+            <br />My name is Vega Setiawan
+            <br />I am Fullstack Developer
+          </h1>
+          <p>i’m specialized in web development, system design, and <br/>Architecture building</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Hero
