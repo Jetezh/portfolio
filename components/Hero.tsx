91e73aaa03622b9d2beee2a215f23c3bd1cc6740
Vega Setiawan <vegasetiawan16@gmail.com>
2025-05-24 09:51:55 +0700

    add cv function

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,6 +7,15 @@ import Link from 'next/link'
 import { SiReaddotcv } from "react-icons/si";
 import { FaSquareGithub } from "react-icons/fa6";
 
+const downloadCV = () => {
+  const cv = '';
+  if (cv !== "") {
+    window.open(cv, '_blank');
+  } else {
+    window.alert("Oops, there is no CV here");
+  }
+}
+
 const Hero = () => {
   return (
     <>
@@ -28,11 +37,13 @@ const Hero = () => {
             <p className='font-normal text-[var(--primary-font-color)] text-[1.2rem] lg:text-[3.5rem] md:text-[3rem] mt-5 lg:mt-10 md:mt-8'>I specialize in web development, system architecture, and scalable system design.</p>
           </motion.div>
           <div className='z-10 mt-5 lg:mt-20 md:mt-10 flex flex-row gap-5 lg:gap-15 md:gap-10'>
-            <button className="px-4 py-2 md:px-8 md:py-4 lg:px-12 lg:py-6 text-[1rem] lg:text-[2.5rem] md:text-[2rem] rounded-lg lg:rounded-3xl md:rounded-2xl bg-[#FF9149] text-white font-semibold transition duration-300 hover:bg-[var(--button-hover)] hover:cursor-pointer hover:text-[#FF9149] border-4 lg:border-8 md:border-6 border-transparent hover:border-[#FF9149] active:scale-90">
-              <Link className='flex flex-row gap-2 lg:gap-7 md:gap-6 items-center' href="">
+            <button 
+              onClick={downloadCV}
+              className="px-4 py-2 md:px-8 md:py-4 lg:px-12 lg:py-6 text-[1rem] lg:text-[2.5rem] md:text-[2rem] rounded-lg lg:rounded-3xl md:rounded-2xl bg-[#FF9149] text-white font-semibold transition duration-300 hover:bg-[var(--button-hover)] hover:cursor-pointer hover:text-[#FF9149] border-4 lg:border-8 md:border-6 border-transparent hover:border-[#FF9149] active:scale-90">
+              <div className='flex flex-row gap-2 lg:gap-7 md:gap-6 items-center'>
                 <SiReaddotcv />
                 Download CV  
-              </Link>
+              </div>
             </button>
             <button className="px-4 py-2 md:px-8 md:py-4 lg:px-12 lg:py-6 text-[1rem] lg:text-[2.5rem] md:text-[2rem] rounded-lg lg:rounded-3xl md:rounded-2xl bg-[var(--bg-button)] text-[var(--primary-font-color)] border-[var(--border-color)] font-semibold transition duration-300 hover:cursor-pointer hover:bg-[var(--github-button-hover)] hover:text-black border-4 lg:border-8 md:border-6 hover:border-[var(--github-border)] active:scale-90">
               <Link className='flex flex-row gap-2 lg:gap-7 md:gap-6 items-center' href="https://github.com/Jetezh">
